19d83fc2c34ea9992ff2254393398d0080fd9d6d
crazyj <crazyj7@gmail.com>
2019-09-30 10:45:24 +0900

    up

---
+++ cpp/rotatev.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    vector<int> v{10,20,30,40,50};
+    int i;
+
+    for(i=0; i<5; ++i)
+    {
+       cout<<v[i]<<"  ";
+    }
+    cout << endl ;
+
+     for(i=0; i<5; ++i)
+     {
+      	rotate(v.rbegin(),v.rbegin()+1,v.rend());
+        cout<<v[0]<<"  ";
+     }
+     cout << endl ;
+}
