4712712e4dd59f449f1cbb8bfe6fc0e5b30ee97b
crazyj7 <crazyj7@gmail.com>
2020-01-24 09:49:23 +0900

    update

--- kaka20/territory_move/territory_move.cpp
+++ kaka20/territory_move/territory_move.cpp
@@ -15,30 +15,86 @@
 #include <unordered_map>
 #include <deque>
 
+//#include <chrono>
+//#include <time.h>
+#define DEBUG_WIN 1
 
+#if (DEBUG_WIN)
+	#include <windows.h>
+#endif
 
 using namespace std;
 
 
-//bool bdebug = true;
-bool bdebug = false;
+// 0=no print, 1=some log, 2=full log
+int idebug = 1;
 
 int n = 0;
 int gheight=0;
 vector<vector<int>> groupmat;
-vector<int> groupmat_one;	// group id list.
 
-unordered_map<int, vector<pair<int, int>>> grouplist; // 그룹번호별 매트릭스상위치(r,c) 리스트.
+//vector<int> groupmat_one;	// group id list.  그룹목록 삭제
+
+//unordered_map<int, vector<pair<int, int>>> grouplist; // 그룹번호별 매트릭스상위치(r,c) 리스트.
+//unordered_map<int, list<pair<int, int>>> grouplist; // 그룹번호별 매트릭스상위치(r,c) 리스트.  value 타입이 vector가 list보다 빠르다.
+unordered_map<int, vector<int>> grouplist; // pair를 삭제. value=i*n+j. 
 
 bool desc(int a, int b) {
 	return a > b;
 }
 
+#if (DEBUG_WIN)
+
+int clock_gettime(int, struct timespec* tv)
+{
+	static int initialized = 0;
+	static LARGE_INTEGER freq, startCount;
+	static struct timespec tv_start;
+	LARGE_INTEGER curCount;
+	time_t sec_part;
+	long nsec_part;
+
+	if (!initialized) {
+		QueryPerformanceFrequency(&freq);
+		QueryPerformanceCounter(&startCount);
+		timespec_get(&tv_start, TIME_UTC);
+		initialized = 1;
+	}
+
+	QueryPerformanceCounter(&curCount);
+
+	curCount.QuadPart -= startCount.QuadPart;
+	sec_part = curCount.QuadPart / freq.QuadPart;
+	nsec_part = (long)((curCount.QuadPart - (sec_part * freq.QuadPart))
+		* 1000000000UL / freq.QuadPart);
+
+	tv->tv_sec = tv_start.tv_sec + sec_part;
+	tv->tv_nsec = tv_start.tv_nsec + nsec_part;
+	if (tv->tv_nsec >= 1000000000UL) {
+		tv->tv_sec += 1;
+		tv->tv_nsec -= 1000000000UL;
+	}
+	return 0;
+}
+
+
+unsigned long long clock_milli() {
+	timespec t;
+	clock_gettime(0, &t);
+	unsigned long long nano = t.tv_sec * 1000000000 + t.tv_nsec;
+	unsigned long long milli = nano / 1000000;
+	return milli;
+}
+#else
+unsigned long long clock_milli() {
+	return 0;
+}
+#endif
+
+
 
 void print_vect(vector<int> v)
 {
-	if (!bdebug)
-		return;
 	for (auto i : v) {
 		cout << i << " ";
 	}
@@ -47,8 +103,6 @@ void print_vect(vector<int> v)
 
 void print_mat(vector<vector<int>> m)
 {
-	if (!bdebug)
-		return;
 	for (auto i : m) {
 		print_vect(i);
 	}
@@ -57,8 +111,6 @@ void print_mat(vector<vector<int>> m)
 
 void print_mat(deque < vector<int>> m)
 {
-	if (!bdebug)
-		return;
 	for (auto i : m) {
 		print_vect(i);
 	}
@@ -121,11 +173,12 @@ vector<pair<int,int>>  update_group(int oldgroupid, int newgroupid) {
 
 int solution(vector<vector<int>> land, int height) {
 	int answer = 0;
+	unsigned long long t1, t2;
 	
 	gheight = height;
 	n = land.size();
 
-	if (bdebug) {
+	if (idebug) {
 		printf("height=%d n=%d\n", gheight, n);
 	}
 	//bool bret = can_move(land, 1, 1, 'r');
@@ -140,25 +193,36 @@ int solution(vector<vector<int>> land, int height) {
 	for (int i = 0; i < n; i++)
 		groupmat.push_back(row);
 
-	if (true)
+	if (idebug)
 		printf("phase1. groupping...\n");
 
 	// 1단계. 그루핑.
 	int groupcnt = 0;
 	int groupidx = 0;	// inc
 	for (int i = 0; i < n; i++) {
+
+		if (idebug) {
+			printf("progress=%.2f  groupcnt=%d\n", ((i + 1.0) / n) * 100.0, groupcnt);
+		}
+		t1 = clock_milli();
+
 		for (int j = 0; j < n; j++) {
+
 			int gi = groupmat[i][j];
 			int gr = 0;
 			int gd = 0;
+
+
 			if (gi == 0) {	// new group
 				groupcnt++;
 				groupidx++;
 				groupmat[i][j] = groupidx;
 				gi = groupidx;
-				groupmat_one.push_back(gi);	
+				//groupmat_one.push_back(gi);	
 
-				grouplist[gi] = vector<pair<int, int>>{ make_pair(i, j) };
+				//grouplist[gi] = vector<pair<int, int>>{ make_pair(i, j) };
+				//grouplist[gi] = list<pair<int, int>>{ make_pair(i, j) };
+				grouplist[gi] = vector<int>{ i * n + j };
 
 			}
 			bool canr = false;
@@ -168,7 +232,8 @@ int solution(vector<vector<int>> land, int height) {
 				if (gr == 0) {
 					groupmat[i][j + 1] = gi;
 					gr = gi;
-					grouplist[gi].push_back(make_pair(i, j + 1));
+					//grouplist[gi].push_back(make_pair(i, j + 1));
+					grouplist[gi].push_back(i * n + j + 1);
 				}
 				else if (gr != gi) {
 					canr = true;
@@ -180,7 +245,8 @@ int solution(vector<vector<int>> land, int height) {
 				if (gd == 0) {
 					groupmat[i + 1][j] = gi;
 					gd = gi;
-					grouplist[gi].push_back(make_pair(i+1, j));
+					//grouplist[gi].push_back(make_pair(i+1, j));
+					grouplist[gi].push_back((i+1) * n + j);
 				}
 				else if (gd != gi) {
 					if (gr != gd) {	// 이미 gr도 해당되기 때문에 groupcnt를 중복해서 줄이면 안됨.
@@ -190,69 +256,44 @@ int solution(vector<vector<int>> land, int height) {
 				}
 			}
 
+
+
 			if (canr || cand ){
-				// 그룹 병합 작업.
-				if (canr) {
-					// gr 그룹 삭제(병합)
-					auto it = find(groupmat_one.begin(), groupmat_one.end(), gr);
-					if (it == groupmat_one.end()) {
-						printf("error!: not found??\n");
-					}
-					groupmat_one.erase(it);
-
-				}
-				if ( cand && gr!=gd ) {
-					// gd 그룹 삭제(병합)
-					auto it = find(groupmat_one.begin(), groupmat_one.end(), gd);
-					if (it == groupmat_one.end()) {
-						printf("error!: not found??\n");
-					}
-					groupmat_one.erase(it);
-				}
-
-				// 여기서 오버헤드가 클 것으로 예상됨!!!!!
-				// 이것을 해결하려면??? 그룹별 위치를 저장해야 한다. map. int: pair<int,int> 
-				/*for (int m1 = 0; m1 < n; m1++) {
-					for (int m2 = 0; m2 < n; m2++) {
-						if (canr) {
-							if (groupmat[m1][m2] == gr)
-								groupmat[m1][m2] = gi;
-						}
-						if (cand && gr!=gd ) {
-							if (groupmat[m1][m2] == gd)
-								groupmat[m1][m2] = gi;
-						}
-					}
-				}*/
 				if (canr) {
 					for (auto itg : grouplist[gr]) {
-						groupmat[itg.first][itg.second] = gi;
-						grouplist[gi].push_back(make_pair(itg.first, itg.second));
-
+						//groupmat[itg.first][itg.second] = gi;
+						groupmat[itg / n][itg % n] = gi;
 					}
+					grouplist[gi].insert(grouplist[gi].end(), grouplist[gr].begin(), grouplist[gr].end());
+					grouplist.erase(gr);
 				}
 				if (cand) {
 					for (auto itg : grouplist[gd]) {
-						groupmat[itg.first][itg.second] = gi;
-						grouplist[gi].push_back(make_pair(itg.first, itg.second));
+						//groupmat[itg.first][itg.second] = gi;
+						groupmat[itg / n][itg % n] = gi;
 					}
+					grouplist[gi].insert(grouplist[gi].end(), grouplist[gd].begin(), grouplist[gd].end());
+					grouplist.erase(gd);
 				}
+			} // end of if can move.
 
-
-			}
+		} // end of for j
+		t2 = clock_milli();
+		if (idebug) {
+			cout << "time=" << t2 - t1 << "ms" << endl;
 		}
-	}
+	} // end of for i
 
-	if (true)
+	if (idebug)
 		printf("group count=%d\n", groupcnt);
 
-	if (bdebug) {
+	if (idebug>1) {
 		printf("input\n");
 		print_mat(land);
 
 		print_mat(groupmat);
 		printf("group list\n");
-		print_vect(groupmat_one);
+		//print_vect(groupmat_one);
 	}
 
 	// make groupmatrix -> 1D
@@ -261,21 +302,23 @@ int solution(vector<vector<int>> land, int height) {
 	//		groupmat_one.push_back(groupmat[m1][m2]);
 	//	}
 	//}
-	int firstgroupid = groupmat_one[0];
-	if (bdebug) {
+	//int firstgroupid = groupmat_one[0];
+	int firstgroupid = groupmat[0][0];
+	if (idebug>1) {
 		printf("first groupid =%d\n", firstgroupid);
 	}
 
 	vector<pair<int, int>> group1 = find_group(firstgroupid);
 
 	// debug
-	if (bdebug) {
+	if (idebug>1) {
 		for (auto it : group1) {
 			printf("(%d,%d) ", it.first, it.second);
 		}
 		printf("\n") ;
 	}
 
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Phase2 . search the lowest cost path.
 	deque<vector<int>> allbridge;
 
@@ -294,9 +337,10 @@ int solution(vector<vector<int>> land, int height) {
 		int mincost = 99999;
 		int mincostgroup = 0;
 
-		if (bdebug)
+		if ( idebug > 1) {
 			printf("group count=%d\n", groupcnt);
-		if (groupcnt % 1000 == 1) {
+		}
+		if (idebug && (groupcnt % 1000 == 1)) {
 			printf("group count=%d\n", groupcnt);
 		}
 
@@ -309,7 +353,7 @@ int solution(vector<vector<int>> land, int height) {
 				gi = -1;
 			else
 				gi = groupmat[it.first][it.second - 1];
-			if (gi > firstgroupid) {
+			if (gi!=-1 && gi != firstgroupid) {
 				cost = abs(land[it.first][it.second] - land[it.first][it.second - 1]);
 				vector<int> tmp{ it.first, it.second, it.first, it.second - 1, gi, cost };
 				allbridge.push_back(tmp);
@@ -320,7 +364,7 @@ int solution(vector<vector<int>> land, int height) {
 				gi = -1;
 			else
 				gi = groupmat[it.first][it.second + 1];
-			if (gi > firstgroupid) {
+			if (gi != -1 && gi != firstgroupid) {
 				cost = abs(land[it.first][it.second] - land[it.first][it.second + 1]);
 				vector<int> tmp{ it.first, it.second, it.first, it.second + 1, gi, cost };
 				allbridge.push_back(tmp);
@@ -331,7 +375,7 @@ int solution(vector<vector<int>> land, int height) {
 				gi = -1;
 			else
 				gi = groupmat[it.first-1][it.second ];
-			if (gi > firstgroupid) {
+			if (gi != -1 && gi != firstgroupid) {
 				cost = abs(land[it.first][it.second] - land[it.first-1][it.second]);
 				vector<int> tmp{ it.first, it.second, it.first-1, it.second, gi, cost };
 				allbridge.push_back(tmp);
@@ -342,13 +386,13 @@ int solution(vector<vector<int>> land, int height) {
 				gi = -1;
 			else
 				gi = groupmat[it.first+1][it.second];
-			if (gi > firstgroupid) {
+			if (gi != -1 && gi != firstgroupid) {
 				cost = abs(land[it.first][it.second] - land[it.first+1][it.second]);
 				vector<int> tmp{ it.first, it.second, it.first+1, it.second, gi, cost };
 				allbridge.push_back(tmp);
 			}
 		}
-		if (bdebug) {
+		if (idebug>1) {
 			printf("all bridge count=%d\n", allbridge.size());
 		}
 		if (allbridge.size() == 0) {
@@ -357,7 +401,7 @@ int solution(vector<vector<int>> land, int height) {
 		}
 
 		// debug
-		if (bdebug) {
+		if (idebug>1) {
 			print_mat(allbridge);
 		}
 
@@ -370,7 +414,7 @@ int solution(vector<vector<int>> land, int height) {
 				idxbr = m1;
 			}
 		}
-		if (bdebug) {
+		if (idebug>1) {
 			printf("mincost=%d mingroup=%d   (%d,%d)->(%d,%d)\n", mincost, mincostgroup,
 				allbridge[idxbr][0], allbridge[idxbr][1], allbridge[idxbr][2], allbridge[idxbr][3]);
 		}
@@ -384,11 +428,19 @@ int solution(vector<vector<int>> land, int height) {
 				m1--;
 			}
 		}
+		//group1 = update_group(mincostgroup, firstgroupid);
+		group1.clear();
+		for (auto itg : grouplist[mincostgroup]) {
+			//group1.push_back(make_pair(itg.first, itg.second));
+			//groupmat[itg.first][itg.second] = firstgroupid;
+			group1.push_back(make_pair(itg / n, itg % n));
+			groupmat[itg / n][itg % n] = firstgroupid;
+		}
+		grouplist.erase(mincostgroup);
 
-		group1 = update_group(mincostgroup, firstgroupid);
+
 		groupcnt--;
 	}
-
 
 
 	return answer;
@@ -414,7 +466,7 @@ int main()
 {
     std::cout << "Hello World!\n";
 	int h = 1;
-	vector<vector<int>> land;
+	groupmat.clear();
 
 	//h = 3, r=15
 	//h=3;
@@ -423,11 +475,12 @@ int main()
 	//h=1, r=18, 
 	//h = 1;
 	//vector< vector<int>> land{ {10, 11, 10, 11},{2, 21, 20, 10},{1, 20, 21, 11},{2, 1, 2, 1} };
-	groupmat.clear();
+
 
 	// 300, 40 ; 103s ; many groups...
 	// 300, 60, 27s	; less group
-	h = 60;
+	h = 30;
+	vector<vector<int>> land;
 	land = make_random(300);
 
 	int t1 = time(NULL);
@@ -436,6 +489,7 @@ int main()
 
 	printf("answer=%d\n", iret);
 	printf("time=%d\n", t2 - t1);
+
 
 	return 0;
 }
